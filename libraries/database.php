0b9842020b399d3a97e6617534fe0b7b66c925a3
WEB3 <WEB3@CRPBeauvoir.fr>
2019-10-22 11:16:16 +0200

    singleton pattern added

--- libraries/database.php
+++ libraries/database.php
@@ -1,7 +1,13 @@
 <?php
 
+$commentaires = $model->findAll();
+$commentaire = $model->find(1);
+$model->delete(1);
+
 
 class Database {
+
+    private static $instance = null;
     /**
  * retourne une connexion à la base de données
  * 
@@ -9,11 +15,16 @@ class Database {
  */
 
 public static function getPdo(): PDO {
-    $pdo = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'root', '', [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-    ]);
-    return $pdo;
+//singleeton...
+    if(self::$instance === null)
+    {
+        self::$instance = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'root', '', [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+
+    return self::$instance;
 }
 }
 
